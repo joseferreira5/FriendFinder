68cdf9370da9d724f801a1e2978a480737f48b5c
Jose Ferreira <jferreira590@gmail.com>
2019-09-24 20:04:46 -0400

    Functioning app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,48 @@
 var friends = require('../data/friends.js');
 
-module.exports = function (app) {
+module.exports = function(app) {
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+  app.get('/api/friends', function(req, res) {
+    return res.json(friends);
+  });
 
-    // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-    app.get("/api/friends", function (req, res) {
-        
-        res.json(friends);
+  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post('/api/friends', function(req, res) {
+    var totalDifference = 0;
+    var bestMatch = {
+      name: '',
+      photo: '',
+      friendDifference: 1000
+    };
+
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+    var intScores = userScores.map(function(item) {
+      return parseInt(item, 10);
     });
 
-    // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-    //app.post("/api/friends", function (req, res) {
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: intScores
+    };
 
-    //});
+    var sum = intScores.reduce((a, b) => a + b, 0);
 
+    for (var i = 0; i < friends.length; i++) {
+      totalDifference = 0;
+
+      var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+      totalDifference += Math.abs(sum - bfriendScore);
+
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+    friends.push(userData);
+    res.json(bestMatch);
+  });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,17 @@
 var path = require('path');
 
-module.exports = function (app) {
+module.exports = function(app) {
+  // A default, catch-all route that leads to home.html which displays the home page.
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
 
-    // A default, catch-all route that leads to home.html which displays the home page.
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+  // A GET Route to /survey which should display the survey page.
+  app.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'));
+  });
 
-    // A GET Route to /survey which should display the survey page.
-    app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'));
+  });
 };
